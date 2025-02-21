c77fedc73cfcc285a951c5ef9c51b04d4be2ad88
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2025-02-21 15:53:05 +0100

    exercise 1.3

--- Part1/Course Information/src/Content.jsx
+++ Part1/Course Information/src/Content.jsx
@@ -1,18 +1,11 @@
 import Part from './Part';
 
-export default function Content({
-	part1,
-	part2,
-	part3,
-	exercises1,
-	exercises2,
-	exercises3,
-}) {
+export default function Content({ part1, part2, part3 }) {
 	return (
 		<>
-			<Part part={part1} exercises={exercises1} />
-            <Part part={part2} exercises={exercises2} />
-			<Part part={part3} exercises={exercises3} />
+			<Part {...part1} />
+			<Part {...part2} />
+			<Part {...part3} />
 		</>
 	);
 }
