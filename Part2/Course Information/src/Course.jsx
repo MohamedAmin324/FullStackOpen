03cf7b3937f931f1f53c64eed76ef49c47b98623
MohamedAmin324 <mohamedaminebofficiel@gmail.com>
2025-02-25 20:38:26 +0100

    step7

--- Part2/Course Information/src/Course.jsx
+++ Part2/Course Information/src/Course.jsx
@@ -1,11 +1,13 @@
 import Header from './Header.jsx';
 import Content from './Content.jsx';
+import Total from './Total.jsx';
 
 function Course({ course: { name, parts } }) {
 	return (
 		<div>
 			<Header name={name} />
 			<Content parts={parts} />
+			<Total parts={parts} />
 		</div>
 	);
 }
